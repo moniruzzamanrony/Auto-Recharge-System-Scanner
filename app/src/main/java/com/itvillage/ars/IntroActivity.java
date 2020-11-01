e73c3d9afd271abd7e8d4c850edc1a296b7c03e5
monirozzamanroni <monirozzaman35-223@diu.edu.bd>
2020-11-02 02:22:12 +0600

    Try to add Server

--- app/src/main/java/com/itvillage/ars/IntroActivity.java
+++ app/src/main/java/com/itvillage/ars/IntroActivity.java
@@ -1,6 +1,7 @@
 package com.itvillage.ars;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -11,10 +12,24 @@ import android.view.animation.AnimationSet;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.security.ProviderInstaller;
 import com.itvillage.ars.ars.R;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.SSLContext;
 
 public class IntroActivity extends AppCompatActivity {
 
+    XammpConnector xammpConnector;
+    Connection conn = null;
     private ImageView logo;
 
     @Override
@@ -37,6 +52,11 @@ public class IntroActivity extends AppCompatActivity {
         animation.addAnimation(fadeOut);
         logo.setAnimation(animation);
 
+        addSSLCertificate();
+
+        xammpConnector = new XammpConnector();
+        Doregister gg = new Doregister();
+        gg.execute("");
 
         new Handler().postDelayed(new Runnable() {
             public void run() {
@@ -44,5 +64,48 @@ public class IntroActivity extends AppCompatActivity {
             }
         }, 3000);
 
+    }
+
+    private void addSSLCertificate() {
+        try {
+            // Google Play will install latest OpenSSL
+            ProviderInstaller.installIfNeeded(getApplicationContext());
+            SSLContext sslContext;
+            sslContext = SSLContext.getInstance("TLSv1.2");
+            sslContext.init(null, null, null);
+            sslContext.createSSLEngine();
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException
+                | NoSuchAlgorithmException | KeyManagementException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public class Doregister extends AsyncTask<String, String, String> {
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            List<String> tableCreateSqlQueryList = new ArrayList<>();
+            // For user_info table
+            tableCreateSqlQueryList.add("CREATE TABLE IF NOT EXISTS user_info (user_id varchar(255),phone_no varchar(255),email varchar(255),shop_name varchar(255),mac_address varchar(255),serial_key varchar(255),active_date varchar(255),expaied_date varchar(255),package_name varchar(255),price varchar(255),client_name varchar(255),initial_password varchar(255),shop_address varchar(255),package_validity varchar(255),role varchar(255));");
+
+            try {
+
+                conn = xammpConnector.CONN();
+
+                for (String sql : tableCreateSqlQueryList) {
+                    System.out.print(sql);
+                    Statement stmt = conn.createStatement();
+                    stmt.execute(sql);
+                    stmt.close();
+                }
+                conn.close();
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+
+            return "Successfully";
+        }
     }
 }
--- app/src/main/java/com/itvillage/ars/XammpConnector.java
+++ app/src/main/java/com/itvillage/ars/XammpConnector.java
@@ -16,7 +16,7 @@ public class XammpConnector {
         try {
 
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://192.168.0.105:3306/auto_recharge_system", "admin", "admin");
+            con = DriverManager.getConnection("jdbc:mysql://167.99.76.96:3306/auto_recharge_system?characterEncoding=utf8", "arm", "L2#j6^%hcyb_?ABAB");
 
         } catch (SQLException e) {
             Log.e("1",e.getMessage());
